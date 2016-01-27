cfa9b3c96a4ccb669114765e6d13fffaf76a256e
Jennifer Bell <jenleebell@gmail.com>
2016-01-27 13:48:53 -0800

    Add material ui and validation to quiz title input

--- client/components/CreateQuiz.js
+++ client/components/CreateQuiz.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import Rebase from 're-base';
 import uuid from 'node-uuid';
+import TextField from 'material-ui/lib/text-field';
 
 const base = Rebase.createClass('https://lumenquiz.firebaseio.com/');
 
+
 export default class CreateQuiz extends React.Component {
+
+constructor(props, context) {
+  super(props, context)
+  this.state = {errorText: null}
+}
+
   render() {
     return (
       <div>
         <h2>Quiz Title</h2>
           <label htmlFor='title'></label>
-          <input type='text' className="style1" placeholder="Enter a title" id='title' ref={(ref) => this.setRef(ref)}/>
+          <TextField hintText="Enter a title" errorText={this.state.errorText} id='title' ref={"quizTitle"} />
           <br />
           <br />
           <button className="btn2" onClick={() => this.handleSubmit()} >Add Questions</button>
@@ -18,30 +26,37 @@ export default class CreateQuiz extends React.Component {
     )
   }
 
-  setRef(ref){
-    this.quizNameRef = ref;
-  }
 
   handleSubmit() {
-    const quizName = this.quizNameRef.value;
-    const quizID = uuid.v4();
-    var questionID;
-    base.post(quizID, {
-      data: {name: quizName}
-    });
-    base.push(`${quizID}/questions`, {
-      data: {content: ''}
-    });
-    base.fetch(`${quizID}`, {
-      context: this,
-      state: 'questions',
-      then(data){
-        questionID = Object.keys(data.questions)[Object.keys(data.questions).length - 1]
-      }
-    });
-    base.push(`${quizID}/questions/${questionID}/answers`, {
-      data: {content: ''}
-    });
-    this.props.history.pushState(null, "/quizzes/" + quizID + '/questions/' + questionID)
+    if (this.refs.quizTitle.getValue() != "") {
+      const quizName = this.refs.quizTitle.getValue();
+      const quizID = uuid.v4();
+      var questionID;
+
+
+
+
+      base.post(quizID, {
+        data: {name: quizName}
+      });
+      base.push(`${quizID}/questions`, {
+        data: {content: ''}
+      });
+      base.fetch(`${quizID}`, {
+        context: this,
+        state: 'questions',
+        then(data){
+          questionID = Object.keys(data.questions)[Object.keys(data.questions).length - 1]
+        }
+      });
+      base.push(`${quizID}/questions/${questionID}/answers`, {
+        data: {content: ''}
+      });
+      this.props.history.pushState(null, "/quizzes/" + quizID + '/questions/' + questionID)
+    } else {
+        this.setState({
+          errorText: "This field is required."
+        });
+    }
   }
 }
