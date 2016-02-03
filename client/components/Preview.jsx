e10252a4c7f51710f4f159d6bdbbcc97a286d3dc
Andrew Haupt <ajhaupt7@gmail.com>
2016-02-03 10:27:33 -0800

    Can delete questions on preview page

--- client/components/Preview.jsx
+++ client/components/Preview.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Rebase from 're-base';
 import PreviewQuestion from './PreviewQuestion.jsx'
+import FlatButton from 'material-ui/lib/flat-button';
 
 const base = Rebase.createClass('https://lumenquiz.firebaseio.com/');
 
@@ -13,16 +14,15 @@ export default class Preview extends React.Component {
   }
 
   componentDidMount(){
-    this.ref = base.fetch(`${this.props.params.quiz_id}/questions`, {
+    this.ref = base.syncState(`${this.props.params.quiz_id}/questions`, {
       context: this,
       state: 'questions',
-      asArray:true,
-      then(data) {
-        this.setState({
-          questions: data
-        })
-      }
+      asArray:true
     });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
   }
 
   questions() {
@@ -35,6 +35,8 @@ export default class Preview extends React.Component {
         content = {question.content}
         quiz_id = {this.props.params.quiz_id}
         history = {this.props.history}
+        question_id = {this.props.params.question_id}
+        deleteQuestion = {(id) => this.deleteQuestion(id)}
       />)
     })}
     return questions
@@ -43,8 +45,39 @@ export default class Preview extends React.Component {
   render() {
     return (
       <div>
+        <FlatButton 
+          className="top-button" 
+          label="Add New Question" 
+          default={true} 
+          onClick={(e) => this.addNewQuestion(e)}
+        />
         {this.questions()}
       </div>
     )
+  }
+
+  deleteQuestion(id) {
+    this.setState({
+      questions: this.state.questions.filter((obj) => {return obj.key !== id})
+    });
+  }
+
+  addNewQuestion() {
+    const quizID = this.props.params.quiz_id
+
+    base.push(`${quizID}/questions`, {
+      data: {content: ''}
+    });
+    base.fetch(`${quizID}`, {
+      context: this,
+      state: 'questions',
+      then(data){
+        const questionID = Object.keys(data.questions)[Object.keys(data.questions).length - 1]
+        base.push(`${quizID}/questions/${questionID}/answers`, {
+          data: {content:'', correct:false}
+        });
+        this.props.history.pushState(null, "/quizzes/" + quizID + '/questions/' + questionID)
+      }
+    });  
   }
 }
--- client/components/PreviewQuestion.jsx
+++ client/components/PreviewQuestion.jsx
@@ -7,8 +7,10 @@ import Paper from 'material-ui/lib/paper';
 import FlatButton from 'material-ui/lib/flat-button';
 import IconButton from 'material-ui/lib/icon-button';
 import EditIcon from 'material-ui/lib/svg-icons/image/edit';
+import Delete from 'material-ui/lib/svg-icons/navigation/cancel';
+import Rebase from 're-base';
 
-
+const base = Rebase.createClass('https://lumenquiz.firebaseio.com/');
 
 const styles = {
   expand: {
@@ -28,11 +30,19 @@ const styles = {
     padding: '15px',
     margin: '10px',
   },
-  editButton: {
-    position:'absolute', 
-    top:'20px', 
-    left:'20px', 
-    color: '#fff'
+  editButtonIcon: {
+    backgroundColor:'#4bbf6b',
+    padding: '5.5px',
+    borderRadius: '100%',
+    width: '1rem',
+    height: '1rem',
+    margin: '2.5px',
+    cursor: 'pointer'
+  },
+  deleteButtonIcon: {
+    width: '2rem',
+    height: '2rem',
+    cursor: 'pointer'
   }
 }
 
@@ -42,10 +52,6 @@ export default class PreviewQuestion extends React.Component {
     this.state = {
       answersOpen: false
     }
-  }
-
-  componentDidMount() {
-
   }
 
   parseHTMLContent() { 
@@ -83,14 +89,19 @@ export default class PreviewQuestion extends React.Component {
       <div>
         <Paper zDepth={2} style={styles.appBar}>
           <div className="question-preview-wrapper">
-            <FlatButton
-              label="Edit"
-              style={styles.editButton}
-              icon={<EditIcon />}
-              hoverColor={'rgba(75, 191, 107, 0.9)'}
-              backgroundColor={'rgba(75, 191, 107, 0.8)'}
-              onClick={() => this.editQuestion()}
-            />
+            <div className="edit-buttons">
+              <EditIcon 
+                style={styles.editButtonIcon}
+                color='#fff' 
+                onClick={() => this.editQuestion()}
+              />
+              <Delete 
+                className = "btn1" 
+                onClick = {() => this.deleteQuestion()}
+                color = {'#c83637'}
+                style = {styles.deleteButtonIcon}
+              />
+            </div>
             {this.parseHTMLContent()}
             {this.renderExpand()}
           </div>
@@ -102,9 +113,15 @@ export default class PreviewQuestion extends React.Component {
     )
   }
 
+  deleteQuestion() {
+    if (confirm("Are you sure you want to delete this question?")) {
+      this.props.deleteQuestion(this.props.id);  
+    }
+  }
+
   editQuestion() {
     const quizID = this.props.quiz_id
-    const  questionID = this.props.id
+    const questionID = this.props.id
     this.props.history.pushState(null, "/quizzes/" + quizID + '/questions/' + questionID)
   }
 
